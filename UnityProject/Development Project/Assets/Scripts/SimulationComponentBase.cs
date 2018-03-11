43489d1bc4e5e83664cc437edb0fbf87ea5f2253
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-03-11 20:50:48 +0000

    Added a simple simulation controller and component base class to be used as a core for the simulation.

---
+++ UnityProject/Development Project/Assets/Scripts/SimulationComponentBase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimulationSystem
+{
+    public class SimulationComponentBase
+    {
+        private SimulationController controller;
+
+        public SimulationController Controller { get { return controller; } }
+
+        public SimulationComponentBase(SimulationController controller)
+        {
+            this.controller = controller;
+        }
+    }
+}
---
+++ UnityProject/Development Project/Assets/Scripts/SimulationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimulationSystem
+{
+    public class SimulationController
+    {
+        private List<SimulationComponentBase> simulationComponents;
+
+        public SimulationController()
+        {
+            simulationComponents = new List<SimulationComponentBase>();
+        }
+
+        /// <summary>
+        /// Get the simulation component of the desired type or NULL if it doesn't exist.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetSimulationComponent<T>() where T : SimulationComponentBase
+        {
+            Type searchType = typeof(T);
+            T searchItem = null;
+
+            foreach (SimulationComponentBase item in simulationComponents)
+            {
+                if (item.GetType() == searchType)
+                {
+                    searchItem = (T)item;
+                    break;
+                }
+            }
+
+            return searchItem;
+        }
+
+        /// <summary>
+        /// Add a simulation component of the desired type to the controller. If an instance of
+        /// the desired type is already instantiated, it will be returned instead.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T AddSimulationComponent<T>() where T : SimulationComponentBase
+        {
+            T item = GetSimulationComponent<T>();
+
+            // If the component is null, create an instance of it and store it
+            if(item == null)
+            {
+                item = (T)Activator.CreateInstance(typeof(T), this);
+                simulationComponents.Add(item);
+            }
+
+            return item;
+        }
+    }
+}
