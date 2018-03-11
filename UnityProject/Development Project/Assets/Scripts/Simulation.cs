87960bdbc91157d94278168b9d096060753a2b2a
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-03-11 21:13:54 +0000

    Added a simulation object which integrates with Unity to pass events to the simulation controller and initialize all the necessary control systems.

---
+++ UnityProject/Development Project/Assets/Scripts/Simulation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SimulationSystem;
+
+public class Simulation : MonoBehaviour
+{
+    private SimulationController controller;
+
+    void Awake()
+    {
+        controller = new SimulationController();
+
+        // Add some core components.
+        controller.AddSimulationComponent<DecisionMaker>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
