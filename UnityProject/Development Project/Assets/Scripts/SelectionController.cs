a7847368652f6cdacc84e9a8d97e5948efba0f0f
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-04-08 15:42:26 +0100

    Added a simple selection controller and component which will pass selection messages to attached components.

---
+++ UnityProject/Development Project/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionController : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask mask;
+    [SerializeField]
+    private Selectable selectable;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera cam = Camera.main;
+
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+
+        bool selectFlag = false;
+
+        if (Physics.Raycast(ray, out hit, 1000.0f, mask))
+        {
+            if (hit.collider != null)
+            {
+                // Try to get the selectable on the hit object
+                Selectable newSelectable = hit.collider.GetComponent<Selectable>();
+
+                // Does the object have a selectable on it?
+                if (newSelectable != null)
+                {
+                    // Set the flag to true because something was selected
+                    selectFlag = true;
+
+                    // Deselect the previous selection
+                    if (selectable != null && selectable != newSelectable)
+                    {
+                        DeselectCurrent();
+                    }
+
+                    newSelectable.Select();
+                    selectable = newSelectable;
+                }
+            }
+        }
+
+        if(!selectFlag)
+        {
+            DeselectCurrent();
+        }
+    }
+
+    private void DeselectCurrent()
+    {
+        // Deselect the current selectable
+        if (selectable != null)
+        {
+            selectable.Deselect();
+            selectable = null;
+        }
+    }
+}
