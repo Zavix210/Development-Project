bebfdd83bf8da37e4b61266093e01ae9e2ca4f2a
targa <guitarga@gmail.com>
2018-04-09 03:32:29 +0100

    Very basic integration with connor video controller

--- UnityProject/Development Project/Assets/Scripts/VideoController.cs
+++ UnityProject/Development Project/Assets/Scripts/VideoController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SimulationSystem;
+using System;
 
 public class VideoController : SimulationComponentBase
 {
+    VideoPlayerLoader _videoPlayer;
     public VideoController(SimulationController controller) : base(controller)
     {
+        _videoPlayer = GameObject.FindObjectOfType<VideoPlayerLoader>();
+        if (!_videoPlayer)
+            Debug.LogError("VideoPlayer not found on scene!");
 
+        _videoPlayer.finishedPlayingCurrentVideo += onVideoFinishedPlaying;
+    }
+
+    private void onVideoFinishedPlaying()
+    {
+        Debug.Log("Video Finished Playing.");
+        //Video finished playing, send message to show UI decisions?
     }
 
     public override bool IsMessageRouteValid(int route)
@@ -25,9 +37,13 @@ public class VideoController : SimulationComponentBase
         // A decision was made (next video?)
         if(message.Route == (int)MessageDestination.DECISION_CHANGE)
         {
-            // When a video has changed, this can be used to play the next video.
-
-            // Consider how to load further videos here? Are all videos loaded into memory yet?
+            
+            // Right now the video is being loaded dynamically. There is only one video instance in the memory.(The video that is currently playing)
+            string url = ""; //filePath
+            int width = 3840;
+            int height = 1920;
+            _videoPlayer.PlayVideo(url, width, height);
+            
         }
     }
 }
