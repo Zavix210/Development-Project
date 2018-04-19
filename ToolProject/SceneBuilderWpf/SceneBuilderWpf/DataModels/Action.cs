afed29163fc384f03b6d80f46161ea2b0cd86d69
Jack Walker <484081@hull.ac.uk>
2018-04-19 17:42:19 +0100

    Changed API to give action the ability to have time + intensity

---
+++ ToolProject/SceneBuilderWpf/SceneBuilderWpf/DataModels/Action.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneBuilderWpf.DataModels
+{
+    public class Action
+    {
+        /// <summary>
+        /// Fire Smoke etc.. 
+        /// </summary>
+        public Actions Actions { get; }
+
+        /// <summary>
+        /// 0 to 100 would be relative fire/smoke intestinty could increase based on time etc.. 
+        /// </summary>
+        public float Intensity { get; }
+        /// <summary>
+        /// Could be left as 0.0 and that could signfy entire time. 
+        /// </summary>
+        public float Time { get; }
+    }
+}
