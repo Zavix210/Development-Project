5df0c05f75c27ee8d9ab150dd5ffe1ff629f5768
Jack Walker <484081@hull.ac.uk>
2018-04-19 16:06:37 +0100

    data objects / API being used on cs side

---
+++ DataLinkProject/API Seperate Classes/Scene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneBuilderWpf.DataModels
+{
+    public class Scene
+    {
+        public Scene()
+        {
+            GeneralSettings = new Settings();
+            Choice = new List<ScenceChoice>();
+        }
+        public string SceneFile { get; set; }
+
+        public Settings GeneralSettings { get; set; }
+
+        public List<ScenceChoice> Choice { get; set; }
+    }
+
+}
