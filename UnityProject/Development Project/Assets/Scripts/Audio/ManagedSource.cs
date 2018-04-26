95fc8a5fe415ed632d94582f9b4e7cc3bc1a3da4
562670 <C.R.Kerwin@hull.ac.uk>
2018-04-26 19:06:31 +0100

    Integrated a simple test script which uses the audio system with a temporary audio file.

--- UnityProject/Development Project/Assets/Scripts/Audio/ManagedSource.cs
+++ UnityProject/Development Project/Assets/Scripts/Audio/ManagedSource.cs
@@ -71,6 +71,8 @@ public class ManagedSource : MonoBehaviour
         source.clip = clip;
         source.loop = looped;
 
+        source.Play();
+
         OnPlayed();
     }
 
---
+++ UnityProject/Development Project/Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimulationSystem;
+
+/// <summary>
+/// This class is a temporary class for testing some simple features of the simulation
+/// </summary>
+public class Test : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip clip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        AudioTest1();
+	}
+
+    private void AudioTest1()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Simulation sim = Simulation.Instance;
+            AudioController ac = sim.Controller.GetSimulationComponent<AudioController>();
+            ac.PlayClip(Vector3.zero, clip, true);
+        }
+    }
+}
